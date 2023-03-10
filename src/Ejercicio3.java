fdb2f873d7162b758867f8db5e121eacc678b14b
Paula Sousa <paulasousa137@gmail.com>
2023-03-10 11:44:16 +0100

    Ejercicio3

---
+++ src/Ejercicio3.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+/*Crea una aplicación que nos pida un número por teclado y con un método se lo pasamos 
+ * por parámetro para que nos indique si es o no un número primo, debe devolver true si
+ *  es primo sino false. Un numero primo es aquel solo puede dividirse entre 1 y si mismo.*/
+
+public class Ejercicio3 {
+	
+	public void numPrimoApp() {
+		
+		boolean primo= false;
+		int num;
+		
+		// Inicializamos el scanner
+				Scanner sc = new Scanner(System.in);
+
+		// Imprimimos el mensaje de bienvenida
+
+				System.out.println("Bienvenido a la App Numeros primos!");
+
+		// Solicitamos el rango de numeros deseado
+				System.out.println("Introduce el número que deseas comprobar: ");
+				num = Integer.parseInt(sc.nextLine());
+				
+		//Llamada al metodo especifico
+				primo = esPrimo(num);
+		//Imprimimos el resultado
+				System.out.println(primo);
+		
+		
+		
+	}
+	
+	public boolean esPrimo(int num) {
+		
+		// Comprobamos que no sean 0, 1 y 4 que no son numeros primos
+		  if (num == 0 || num == 1 || num == 4) {
+		    return false;
+		  }
+		  //Comprobamos que no sea divisible por todos los numeros hasta llegar a su valor
+		  for (int i = 2; i < num; i++) {
+		    
+		    if (num % i == 0)
+		      return false;
+		  }
+		  // Si no se pudo dividir por ninguno de los de arriba,  es primo
+		  return true;
+	}
+	
+
+}
--- src/Ud6App.java
+++ src/Ud6App.java
@@ -3,13 +3,16 @@ public class Ud6App {
 
 	public static void main(String args[]) {
 
-		// Instanciamos y llamamos a los metodos
+// Instanciamos y llamamos a los metodos
 
 		Ejercicio1 ej1 = new Ejercicio1();
 //		ej1.calcularArea();
 
 		Ejercicio2 ej2 = new Ejercicio2();
-		ej2.generaApp();
+//		ej2.generaApp();
+		
+		Ejercicio3 ej3 = new Ejercicio3();
+		ej3.numPrimoApp();
 
 	}
 
