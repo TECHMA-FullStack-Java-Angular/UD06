4311a4693e832b86749c3c9a121af60826d8dc0e
Paula Sousa <paulasousa137@gmail.com>
2023-03-12 13:53:49 +0100

    Ejercicio12

--- src/Ejercicio12.java
+++ src/Ejercicio12.java
@@ -11,6 +11,8 @@ public class Ejercicio12 {
 	// Inicializamos variables
 	int numArr[];
 	int acabaEn;
+	int nuevoArr[];
+	int contador = 0;
 
 	// Abrimos el Scanner
 	Scanner sc = new Scanner(System.in);
@@ -24,45 +26,63 @@ public class Ejercicio12 {
 		// Solicitamos que introduzca el tamaño deseado del array parseado a int
 		System.out.println("Introduzca el tamaño del array a crear: ");
 		numArr = new int[Integer.parseInt(sc.nextLine())];
+		nuevoArr = new int[numArr.length];
 
 		try {
 		// Solicitamos que introduzca el digito que acabaran los numeros introducidos en
 		// el array
 		System.out.println("Introduzca el digito que quieres que acaben los numeros introducidos en el array: ");
 		int input = Integer.parseInt(sc.nextLine());
-			if (input >= 0) {
-				acabaEn = input;
-				rellenarArray(acabaEn, numArr);
-				System.out.println(Arrays.toString(numArr));
-			}
-		}
-		catch (Exception e) {
+		if (input >= 0) {
+			acabaEn = input;
+			rellenarArray(numArr);
+			nuevoArr = nuevoArrayTerminadoEn(acabaEn, numArr, nuevoArr, contador);
+			System.out.println("El nuevo array creada con los numeros acabados en  " + acabaEn
+					+ " originado a partir del array aleatorio anterior son: " + Arrays.toString(nuevoArr)+".");
+		}else {
 			System.out.println("El digito introducido no es correcto. Introduzca un valor numerico positivo.");
-			
 		}
-			
+		} catch (Exception e) {
+			System.out.println("El digito introducido no es correcto. Introduzca un valor numerico positivo.");
 
-		
+		}
 
 	}
 
 	// Metodo para rellenar array con numeros aleatorios de de rango especifico
-	public void rellenarArray(int num, int[] numArr) {
+	public void rellenarArray(int[] numArr) {
 
 		// Rellenamos el array según el tamaño indicado con numeros aleatorios
 		// No ha solicitado que los numeros no se repitan
 		for (int i = 0; i < numArr.length; i++) {
-			int iteracion = (int) (Math.floor(Math.random() * 300) + 1);
-
-			// Comprobamos que el numero acabe en el digito indicado
-			if (iteracion % 10 == num) {
-				numArr[i] = iteracion;
-
-			}else {
-				i--;
-			}
+			numArr[i] = (int) (Math.floor(Math.random() * 300) + 1);
 
 		}
+		System.out.println("Array aleatoria: " + Arrays.toString(numArr));
 	}
 
+	public int[] nuevoArrayTerminadoEn(int terminacion, int[] num, int[] nuevoArray, int contador) {
+
+		for (int i = 0; i < num.length; i++) {
+			// Comprobamos que el numero acabe en el digito indicado
+			if (num[i] % 10 == terminacion) {
+				contador++;
+			} else {
+				num[i] = 0;
+			}
+		}
+
+		// Crear un nuevo array con la cantidad correcta de elementos
+		nuevoArray = new int[contador];
+
+		// Copiar solo los elementos no cero de num en nuevoArray
+		int index = 0;
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] != 0) {
+				nuevoArray[index] = num[i];
+				index++;
+			}
+		}
+		return nuevoArray;
+	}
 }
